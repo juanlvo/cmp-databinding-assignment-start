c0ccf5619a070a8a147a277a0f91f7484d13f965
Juan Vivas <juanlvo@gmail.com>
2019-02-06 11:48:05 +0100

    feat: Setp 3

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
-
+  @Output() startGame = new EventEmitter<{i: number}>();
+  count;
   constructor() { }
 
   ngOnInit() {
+    this.count = 0;
+  }
+
+  startTheGame() {
+    setInterval(() => {
+      this.count++;
+      this.startGame.emit({ i: this.count});
+      console.log(this.count);
+    },
+    this.count);
   }
 
 }
